e2569d7b6b9d0afcee782936abf4a837480f9472
Nirmal <nirmalkumar07781@gmail.com>
2023-07-13 00:16:01 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -66,6 +66,19 @@ st.set_page_config(
                     page_icon = 'Related Images and Videos/Logo.png'
                     )
 
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+
 st.title(':blue[PhonePe Data Visualization]')
 
 add_vertical_space(2)
